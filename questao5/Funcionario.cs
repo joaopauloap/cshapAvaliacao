b7cfb03e74099bebedfca105c91d68996da6489b
João Paulo de Araújo Porto <joaopaulo.porto@foursys.com.br>
2023-08-15 15:30:31 -0300

    first commit

---
+++ questao5/Funcionario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace questao5
+{
+    public class Funcionario
+    {
+        public double HorasTrabalhadas { get; set; }
+        public double ValorHora { get; set; }
+
+        public double Salario()
+        {
+            return (HorasTrabalhadas * ValorHora);
+        }
+
+        public void MostrarSalario()
+        {
+            Console.WriteLine($"O salário a ser pago é: R$ {(HorasTrabalhadas*ValorHora).ToString("C")}");
+        }
+    }
+}
---
+++ questao6/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace questao6
+{
+    public class Produto
+    {
+        public int Quantidade { get; set; }
+        public double Preco { get; set; }
+        public double Desconto { get; set; }
+
+        public double Total()
+        {
+            return (Quantidade*(Preco-Desconto));
+        }
+    }
+}
